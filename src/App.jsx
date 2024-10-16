83a70ad124a3642c40e6d8a2227a4bdef5f14baf
Aditya Garga <adigarga@gmail.com>
2024-10-16 11:10:40 +0200

    changed text

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 function App() {
     return (
-        <>
-            <div>hello world</div>
-        </>
+      <>
+        <p>This is a simple React application.</p>
+        <p>Here is some more text to display on the page.</p>
+        <ul>
+          <li>First item</li>
+          <li>Second item</li>
+          <li>Third item</li>
+        </ul>
+        <p>Feel free to add more content as needed.</p>
+      </>
     );
 }
 
